81b3ecf05084c8db489aaf739a60d54d44c3d4b8
Ricky L <75475136+rickylai248@users.noreply.github.com>
2021-04-01 19:37:02 -0700

    Add files via upload

---
+++ sortingalgorithm.py
@@ -0,0 +1,15 @@
+# Sorting Algorithm Visualizer
+# Inspired throughout my completion in our Data Structures and Algorithm's course during university
+
+# Beginning with sorting algorithms we learned first in-class which is inclusive of:
+#   Selection Sort
+#   Insertion Sort
+#   Quick Sort
+#   Merge Sort
+# and more to come!
+
+#import sorting_algorithms, time, os, sys
+#os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+#import pygame
+
+#testing 123
